57f085f554e3a5fe073c157706e6e9d0a4a263ce
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-13 15:52:36 -0600

    add correct assignment with bonus tax rate calculation

--- Salary-Calculator/Program.cs
+++ Salary-Calculator/Program.cs
@@ -10,24 +10,64 @@ namespace Salary_Calculator
     {
         static void Main(string[] args)
         {
-            int num;
-            Console.Write("Enter an integer between 0 and 75: ");
-            num = Convert.ToInt32(Console.ReadLine());
-            if (num < 0)
+            double hourlyRate;
+            double hoursWorked;
+            double overtimeHours;
+            double basePay;
+            double overtimePay;
+            double totalPay;
+            string userInput;
+            double taxRate;
+            double taxAmount = 0.00;
+            Boolean userConfirm = false;
+
+            Console.Write("Please enter the hourly rate for this employee: ");
+            hourlyRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter the hours worked by this employee: ");
+            hoursWorked = Convert.ToDouble(Console.ReadLine());
+
+            if (hoursWorked > 40.00)
             {
-                Console.WriteLine("That number is too small.");
-            } else if (num > 75)
-            {
-                Console.WriteLine("That number is too large.");
+                overtimeHours = hoursWorked - 40.00;
+                hoursWorked = 40.00;
+
+                basePay = hoursWorked * hourlyRate;
+                overtimePay = overtimeHours * (hourlyRate * 1.50);
+
+                totalPay = Math.Round(basePay + overtimePay, 2, MidpointRounding.AwayFromZero);
             } else
             {
-                for (int i = 0; i < num; i++)
+                totalPay = Math.Round(hoursWorked * hourlyRate, 2, MidpointRounding.AwayFromZero);
+            }
+
+            while (userConfirm == false) {
+                Console.Write("Does this employee need taxes withheld from their pay? (Y/N): ");
+                userInput = Console.ReadLine().ToUpper();
+
+                if (userInput == "Y")
                 {
-                    Console.Write("*");
+                    Console.Write("Please enter the tax rate as an integer: ");
+                    taxRate = Convert.ToDouble(Console.ReadLine());
+                    taxRate /= 100;
+                    taxAmount = Math.Round(totalPay * taxRate, 2 , MidpointRounding.AwayFromZero);
+                    totalPay -= taxAmount;
+                    break;
+                } else if (userInput == "N")
+                {
+                    break;
+                } else
+                {
+                    Console.WriteLine("Please enter a valid selection.");
                 }
             }
 
-            Console.WriteLine("\nPress 'enter' to continue.");
+            Console.WriteLine("Pay for the employee is: $" + totalPay.ToString("F"));
+            if (taxAmount != 0.00)
+            {
+                Console.WriteLine("The employee had $" + taxAmount.ToString("F") + " witheld from their paycheck.");
+            }
+            Console.WriteLine("Press 'enter' to exit the progarm.");
             Console.ReadLine();
         }
     }
