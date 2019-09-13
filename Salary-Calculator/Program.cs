1ae83030407313220bb995818ea398ed92bfc99b
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-13 15:03:11 -0600

    add the logic for a seperate assignment

--- Salary-Calculator/Program.cs
+++ Salary-Calculator/Program.cs
@@ -10,7 +10,25 @@ namespace Salary_Calculator
     {
         static void Main(string[] args)
         {
+            int num;
+            Console.Write("Enter an integer between 0 and 75: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num < 0)
+            {
+                Console.WriteLine("That number is too small.");
+            } else if (num > 75)
+            {
+                Console.WriteLine("That number is too large.");
+            } else
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write("*");
+                }
+            }
 
+            Console.WriteLine("\nPress 'enter' to continue.");
+            Console.ReadLine();
         }
     }
 }
